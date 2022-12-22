d4bd81fab2206e8fa5ac9da871ff6b194bb80b2b
stanlymathai <stanly@mathai.com>
2022-12-22 17:57:21 +0530

    chore; modify comment controller

--- backend/module/comment/controller/commentsController.js
+++ backend/module/comment/controller/commentsController.js
@@ -1,55 +1,24 @@
 const Comment = require('../model/commentsModel');
 
 const addComment = (req, res) => {
-  let data;
-  if (req.body.payload) {
-    data = req.body.payload;
-  } else {
-    data = {
-      userId: Math.random(),
-      comId: Math.random(),
-      fullName: Math.random(),
-      text: Math.random(),
-      avatarUrl:
-        'https://s3.eu-west-2.amazonaws.com/prod-monitalks-media/userplaceholder_5734b83bd0.png',
-      timeStamp: new Date(),
-      replies: [],
-    };
-  }
-  const comment = new Comment(data);
+  const comment = new Comment(req.body.payload);
   comment
     .save()
-    .then((comment) =>
-      res.json({
-        comment: comment,
-      })
-    )
-    .catch((err) => res.status(500).json({ error: err }));
-};
-
-const updateComment = (req, res) => {
-  let comment = req.body;
-  Comment.updateOne(
-    { _id: comment.id },
-    { $set: { commentText: comment.commentText } }
-  )
-    .exec()
-    .then((result) =>
-      res.status(200).json({
-        message: 'Comment Updated',
-        comment: comment,
-      })
-    )
+    .then((res) => {
+      if (res)
+        res.json({
+          message: 'Comment Added',
+        });
+    })
     .catch((err) => res.status(500).json({ error: err }));
 };
 
 const getComments = (req, res) => {
-  console.log('hai from getComments', Comment);
+  let articleId = req.params.articleId;
   Comment.find({})
     .lean()
     .exec()
     .then((comments) => {
-      console.log(comments, ' getComments');
 
       return res.json(comments);
       let rec = (comment, threads) => {
@@ -88,6 +57,5 @@ const getComments = (req, res) => {
 
 module.exports = {
   addComment,
-  updateComment,
   getComments,
 };
--- backend/module/comment/router/comments.js
+++ backend/module/comment/router/comments.js
@@ -2,8 +2,7 @@ let express = require('express');
 let router = express.Router();
 const commentsController = require('../controller/commentsController');
 
-router.get('/', commentsController.getComments);
+router.get('/:articleId', commentsController.getComments);
 router.post('/', commentsController.addComment);
-router.post('/update', commentsController.updateComment);
 
 module.exports = router;
